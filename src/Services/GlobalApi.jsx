5987600b1504bafdeb7e989d86c29a789fd4deb0
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-04-11 02:48:21 -0700

    feat: add genre-based movie list with scrollable cards
    - Added GenreMovieList, MovieList, and MovieCard components
    - Integrated TMDB API for genre-specific movie fetch
    - Updated App.jsx and GlobalApi.jsx accordingly

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,9 +3,14 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "a0f3c30fe009b6c0e70d04071aef0fa1";
 
+
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=a0f3c30fe009b6c0e70d04071aef0fa1';
+
 export default {
   getTrendingMovies: () => 
-    axios.get(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`)
+    axios.get(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`),
+  getMovieByGenreId : (id)=> axios.get(`${movieByGenreBaseURL}&with_genres=${id}`)
+  
 };
 
 // export const customFetch = (axios.create({
